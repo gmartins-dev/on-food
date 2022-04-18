db10b6964103bce8b03b2361209c472798ecd18f
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-04-18 13:29:51 -0300

    api updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,21 @@ import { QueryClient, QueryClientProvider } from "react-query"
 
 function MyApp({ Component, pageProps }: AppProps) {
   
-  const [queryClient] = useState(new QueryClient());
+  const [queryClient] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+        },
+      },
+  }));
+  
   
   return (
   <QueryClientProvider client={queryClient}>
   <Component {...pageProps} />
   </QueryClientProvider>
   );
-}
+};
 
 export default MyApp
